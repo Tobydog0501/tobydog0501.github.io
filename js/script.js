d71813f4504cd286307158088354b777e56730c3
tobydog0501 <tobydog0501@gmail.com>
2024-02-07 11:09:27 +0800

    test iframe

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ window.onload = ()=>{
 // 	document.styleSheets[0].disabled = true;
 // 	document.styleSheets[1].disabled = false;
 // }
+
+function serverStat(){
+	fetch(`${window.location.origin}/content/serverStat.html`)
+		.then((res)=>res.text().then(text=>{
+			document.getElementById("container").innerHTML = text;
+		}))
+}
+
+function externalStat(){
+	document.getElementById("container").innerHTML = text;
+}
